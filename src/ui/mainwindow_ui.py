314765c7249f62d886916c0810d498f1c6012a66
minimirror1 <minimirror1@gmail.com>
2025-02-05 14:37:42 +0900

    아이콘

--- src/ui/mainwindow_ui.py
+++ src/ui/mainwindow_ui.py
@@ -15,31 +15,175 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCommandLinkButton, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QToolButton,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QStatusBar,
+    QVBoxLayout, QWidget)
+import _icons_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(640, 480)
+        MainWindow.resize(962, 700)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.pushButton = QPushButton(self.centralwidget)
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.leftMenuSubContainer = QWidget(self.centralwidget)
+        self.leftMenuSubContainer.setObjectName(u"leftMenuSubContainer")
+        self.leftMenuSubContainer.setMinimumSize(QSize(200, 0))
+        self.leftMenuSubContainer.setMaximumSize(QSize(200, 16777215))
+        self.verticalLayout = QVBoxLayout(self.leftMenuSubContainer)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalWidget = QWidget(self.leftMenuSubContainer)
+        self.horizontalWidget.setObjectName(u"horizontalWidget")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.horizontalWidget.sizePolicy().hasHeightForWidth())
+        self.horizontalWidget.setSizePolicy(sizePolicy)
+        self.horizontalLayout_2 = QHBoxLayout(self.horizontalWidget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.menuButton = QPushButton(self.horizontalWidget)
+        self.menuButton.setObjectName(u"menuButton")
+        self.menuButton.setMinimumSize(QSize(50, 0))
+        self.menuButton.setMaximumSize(QSize(50, 16777215))
+
+        self.horizontalLayout_2.addWidget(self.menuButton)
+
+
+        self.verticalLayout.addWidget(self.horizontalWidget)
+
+        self.verticalFrame = QFrame(self.leftMenuSubContainer)
+        self.verticalFrame.setObjectName(u"verticalFrame")
+        self.verticalFrame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.verticalFrame.setFrameShadow(QFrame.Shadow.Raised)
+        self.verticalLayout_7 = QVBoxLayout(self.verticalFrame)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.HomeButton = QPushButton(self.verticalFrame)
+        self.HomeButton.setObjectName(u"HomeButton")
+
+        self.verticalLayout_7.addWidget(self.HomeButton)
+
+        self.PlayButton = QPushButton(self.verticalFrame)
+        self.PlayButton.setObjectName(u"PlayButton")
+
+        self.verticalLayout_7.addWidget(self.PlayButton)
+
+        self.jogButton = QPushButton(self.verticalFrame)
+        self.jogButton.setObjectName(u"jogButton")
+
+        self.verticalLayout_7.addWidget(self.jogButton)
+
+
+        self.verticalLayout.addWidget(self.verticalFrame)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.verticalFrame_2 = QFrame(self.leftMenuSubContainer)
+        self.verticalFrame_2.setObjectName(u"verticalFrame_2")
+        self.verticalFrame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.verticalLayout_8 = QVBoxLayout(self.verticalFrame_2)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.SettingButton = QPushButton(self.verticalFrame_2)
+        self.SettingButton.setObjectName(u"SettingButton")
+
+        self.verticalLayout_8.addWidget(self.SettingButton)
+
+        self.HelpButton = QPushButton(self.verticalFrame_2)
+        self.HelpButton.setObjectName(u"HelpButton")
+
+        self.verticalLayout_8.addWidget(self.HelpButton)
+
+
+        self.verticalLayout.addWidget(self.verticalFrame_2)
+
+
+        self.horizontalLayout.addWidget(self.leftMenuSubContainer)
+
+        self.centerMenuContainer = QWidget(self.centralwidget)
+        self.centerMenuContainer.setObjectName(u"centerMenuContainer")
+        self.centerMenuContainer.setMinimumSize(QSize(200, 0))
+        self.centerMenuContainer.setMaximumSize(QSize(200, 16777215))
+        self.verticalLayout_5 = QVBoxLayout(self.centerMenuContainer)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+
+        self.horizontalLayout.addWidget(self.centerMenuContainer)
+
+        self.mainBodyContainer = QWidget(self.centralwidget)
+        self.mainBodyContainer.setObjectName(u"mainBodyContainer")
+        self.verticalLayout_3 = QVBoxLayout(self.mainBodyContainer)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.headerContainer = QWidget(self.mainBodyContainer)
+        self.headerContainer.setObjectName(u"headerContainer")
+        self.headerContainer.setMinimumSize(QSize(0, 0))
+        self.headerContainer.setMaximumSize(QSize(16777215, 30))
+        self.horizontalLayout_3 = QHBoxLayout(self.headerContainer)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.tempFrame = QFrame(self.headerContainer)
+        self.tempFrame.setObjectName(u"tempFrame")
+        self.horizontalLayout_6 = QHBoxLayout(self.tempFrame)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+
+        self.horizontalLayout_3.addWidget(self.tempFrame)
+
+        self.windowBtnFrame = QFrame(self.headerContainer)
+        self.windowBtnFrame.setObjectName(u"windowBtnFrame")
+        self.horizontalLayout_5 = QHBoxLayout(self.windowBtnFrame)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.minimizeBtn = QPushButton(self.windowBtnFrame)
+        self.minimizeBtn.setObjectName(u"minimizeBtn")
+        icon = QIcon()
+        icon.addFile(u":/feather/icons/feather/window_minimize.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.minimizeBtn.setIcon(icon)
+
+        self.horizontalLayout_5.addWidget(self.minimizeBtn)
+
+        self.restoreBtn = QPushButton(self.windowBtnFrame)
+        self.restoreBtn.setObjectName(u"restoreBtn")
+        icon1 = QIcon()
+        icon1.addFile(u":/feather/icons/feather/window_undock.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.restoreBtn.setIcon(icon1)
+
+        self.horizontalLayout_5.addWidget(self.restoreBtn)
+
+        self.closeBtn = QPushButton(self.windowBtnFrame)
+        self.closeBtn.setObjectName(u"closeBtn")
+        icon2 = QIcon()
+        icon2.addFile(u":/feather/icons/feather/x.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.closeBtn.setIcon(icon2)
+
+        self.horizontalLayout_5.addWidget(self.closeBtn)
+
+
+        self.horizontalLayout_3.addWidget(self.windowBtnFrame)
+
+
+        self.verticalLayout_3.addWidget(self.headerContainer)
+
+        self.homePage = QFrame(self.mainBodyContainer)
+        self.homePage.setObjectName(u"homePage")
+        self.homePage.setMinimumSize(QSize(0, 0))
+        self.homePage.setFrameShape(QFrame.Shape.StyledPanel)
+        self.horizontalLayout_4 = QHBoxLayout(self.homePage)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.pushButton = QPushButton(self.homePage)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(190, 180, 75, 24))
-        self.toolButton = QToolButton(self.centralwidget)
-        self.toolButton.setObjectName(u"toolButton")
-        self.toolButton.setGeometry(QRect(220, 270, 24, 22))
-        self.commandLinkButton = QCommandLinkButton(self.centralwidget)
-        self.commandLinkButton.setObjectName(u"commandLinkButton")
-        self.commandLinkButton.setGeometry(QRect(310, 190, 172, 41))
+
+        self.horizontalLayout_4.addWidget(self.pushButton)
+
+
+        self.verticalLayout_3.addWidget(self.homePage)
+
+
+        self.horizontalLayout.addWidget(self.mainBodyContainer)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 640, 33))
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -51,8 +195,15 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.menuButton.setText(QCoreApplication.translate("MainWindow", u"menu", None))
+        self.HomeButton.setText(QCoreApplication.translate("MainWindow", u"Home", None))
+        self.PlayButton.setText(QCoreApplication.translate("MainWindow", u"Play", None))
+        self.jogButton.setText(QCoreApplication.translate("MainWindow", u"Jog", None))
+        self.SettingButton.setText(QCoreApplication.translate("MainWindow", u"Setting", None))
+        self.HelpButton.setText(QCoreApplication.translate("MainWindow", u"Help", None))
+        self.minimizeBtn.setText("")
+        self.restoreBtn.setText("")
+        self.closeBtn.setText("")
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.toolButton.setText(QCoreApplication.translate("MainWindow", u"...", None))
-        self.commandLinkButton.setText(QCoreApplication.translate("MainWindow", u"CommandLinkButton", None))
     # retranslateUi
 
